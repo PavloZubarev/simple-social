76cda064631591b5def6c24e7c4fdcc5cc265324
Pavlo <pavlozubarev@gmail.com>
2019-01-22 19:10:26 +0200

    Added posts scroll on group page

--- accounts/views.py
+++ accounts/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.urls import reverse_lazy
 from . import forms
 from django.views.generic import CreateView
-# Create your views here.
+
 
 
 class SignUp(CreateView):
